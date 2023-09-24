a8a1b5c171160661936cdf2489810757f3007536
Thiago-Almeida23 <thiagocldealmeida@gmail.com>
2023-09-24 17:18:49 -0300

    Basic Angular project for the Santander Bootcamp.

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit{
+
+  @Input()
+  gameCover: string = ""
+  @Input()
+  gameLabel: string = ""
+  @Input()
+  gameType: string = "Digital PS4"
+  @Input()
+  gamePrice: string = "R$ 129,99"
+
+  constructor () {}
+
+  ngOnInit(): void{
+
+  }
+
+}
